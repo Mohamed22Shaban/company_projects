6edfdaeb416a04f427eb80fa1f225915c43d7540
MohamedShaban <mohamedtelb200@gmail.com>
2022-11-27 03:52:37 +0200

    update

--- tasks/admin.py
+++ tasks/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import *
-# admin.site.register(Category)
+admin.site.register(Clients)
 # admin.site.register(Project)
 admin.site.register(EmailSender)
 admin.site.register(EmailSubscribe)
