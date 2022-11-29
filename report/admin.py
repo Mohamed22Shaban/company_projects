60a9ba13fb7c4da33bd30533119217ca16c999c4
MohamedShaban <mohamedtelb200@gmail.com>
2022-11-29 02:50:50 +0200

    update

--- report/admin.py
+++ report/admin.py
@@ -3,13 +3,12 @@ from report.models import OrderReport
 from tasks.models import Project,Order
 from django.template.response import TemplateResponse
 from django.utils.translation import gettext_lazy as _
-from django.db.models.functions import ExtractYear, ExtractMonth, ExtractWeek
+
+from django.db.models.functions import ExtractYear
 from django.db.models import Sum
 import json
-
-
 @admin.register(OrderReport)
-class ProjectAdmin(admin.ModelAdmin):
+class OrderAdmin(admin.ModelAdmin):
 
     change_list_template = 'admin/reports/report.html'
 
@@ -22,34 +21,34 @@ class ProjectAdmin(admin.ModelAdmin):
     def changelist_view(self, request, extra_context=None):
 
         yearly_stats = (
-            Order.objects.select_related('sales')
+            Order.objects.select_related('sales')  # to bring amount
                 .annotate(year=ExtractYear('created_at'))
                 .values('year')
                 .annotate(sum=Sum('sales__amount'))
         )
 
-        monthly_stats = (
-            Order.objects.select_related('sales')
-                .annotate(year=ExtractYear('created_at'))
-                .annotate(month=ExtractMonth('created_at'))
-                .values('year', 'month')
-                .annotate(sum=Sum('sales__amount'))[:30]
-        )
+    #     monthly_stats = (
+    #         Order.objects.select_related('sales')
+    #             .annotate(year=ExtractYear('created_at'))
+    #             .annotate(month=ExtractMonth('created_at'))
+    #             .values('year', 'month')
+    #             .annotate(sum=Sum('sales__amount'))[:30]
+    #     )
 
-        weekly_stats = (
-            Order.objects.select_related('sales')
-                .annotate(year=ExtractYear('created_at'))
-                .annotate(week=ExtractWeek('created_at'))
-                .values('year', 'week')
-                .annotate(sum=Sum('sales__amount'))[:30]
-        )
+    #     weekly_stats = (
+    #         Order.objects.select_related('sales')
+    #             .annotate(year=ExtractYear('created_at'))
+    #             .annotate(week=ExtractWeek('created_at'))
+    #             .values('year', 'week')
+    #             .annotate(sum=Sum('sales__amount'))[:30]
+    #     )
 
         context = {
             **self.admin_site.each_context(request),
             'yearly_stats': json.dumps(list(yearly_stats)),
-            'monthly_stats': json.dumps(list(monthly_stats)),
-            'weekly_stats': json.dumps(list(weekly_stats)),
-            'title': _('Projects Report')
+            # 'monthly_stats': json.dumps(list(monthly_stats)),
+            # 'weekly_stats': json.dumps(list(weekly_stats)),
+            # 'title': _('Projects Report')
         }
 
         return TemplateResponse(
