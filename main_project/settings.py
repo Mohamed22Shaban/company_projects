7385ed1555002a7c37ac1278c74ff721bb0bdd9c
MohamedShaban <mohamedtelb200@gmail.com>
2022-12-12 07:22:58 +0200

    update

--- main_project/settings.py
+++ main_project/settings.py
@@ -20,6 +20,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # BASE_DIR = Path(__file__).resolve().parent.parent
 #   git push heroku main
+DISABLE_COLLECTSTATIC = True
 
 
 ## language settings
@@ -37,7 +38,7 @@ SECRET_KEY = '3Ef5D3C5C5Eb8Bf1D3181410Bb646806A26B51D1C2B604D7'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'fathy20.herokuapp.com//','127.0.0.1'
+    'project-django-show.herokuapp.com/','127.0.0.1'
     ]
 
 
@@ -107,10 +108,10 @@ WSGI_APPLICATION = 'main_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd8b34cjiudmvib',
-        'HOST': 'ec2-3-219-229-143.compute-1.amazonaws.com',
-        'USER': 'pnffahgqlpnayn',
-        'PASSWORD': '58bcaf83d0bbf51bb2bb4d310840ef446d201d712cca260b78c440e4744ad02d',
+        'NAME': 'd8pr5n3187u3pb',
+        'HOST': 'ec2-44-195-132-31.compute-1.amazonaws.com',
+        'USER': 'mqbjyfjzqqropz',
+        'PASSWORD': '2b3008755b60ee2c5122d61e212a45410e47027278e5c7e30327e025f28cad08',
         'PORT': '5432',
     }
 }
@@ -178,7 +179,7 @@ STATICFILES_DIRS = [
 
 ## heroku settings
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
